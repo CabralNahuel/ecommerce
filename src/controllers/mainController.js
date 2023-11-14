2e8dae22b1d654e6312413b5f7e840d96ef43d22
CabralNahuel <cabralnahuel.dev@gmail.com>
2023-11-14 19:54:36 -0300

    Revert "Merge pull request #8 from CabralNahuel/mision-4"
    This reverts commit e77300281bb9fa858a0f15e1659096085d55851c, reversing
    changes made to ac37afb1514bf63edb2746657767602b71a8cb28.

--- src/controllers/mainController.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    home : (req,res)  => res.send("hola desde el home"),
-    contact : (req,res)  =>  res.send("hola desde contact"),
-    about : (req,res)  => res.send("hola desde about"),
-    faqs : (req,res)  => res.send("hola desde faqs")
-}
